acf9be2bdd7c475c7d20edf1ff3c6448b4fc6ba2
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-01-22 14:30:56 +0530

    handle data data modeling and post request

--- Module 6/routers/src/Pages/Contact/Contact.jsx
+++ Module 6/routers/src/Pages/Contact/Contact.jsx
@@ -7,6 +7,7 @@ function Contact() {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+  const [name, setName] = useState("");
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -25,11 +26,36 @@ function Contact() {
     fetchUserData();
   }, []);
 
+  // post request 
+  const handleUser = () => {
+    const newUser = {
+      name,
+      id: crypto.randomUUID(),
+      email: `${name}@gamil.com`,
+      address: {
+        city: "Delhi",
+      },
+    };
+    setUsers([newUser, ...users]);
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newUser)
+      .then((res) => setUsers([res.data, ...users]))
+      .catch((error) => {
+        setError(error.message);
+        setUsers(users);
+      });
+  };
+
   return (
     <div>
+      <div>
+        <input type="text" value={name} onChange={(e) => setName(() => e.target.value)} />
+        <button onClick={handleUser}>Add new user</button>
+      </div>
       <h1>Contact us</h1>
-      {error && <em>{error}</em>}
       {isLoading && <Loader />}
+      {error && <em>{error}</em>}
       <div className="User-container">
         {users.map((user) => (
           <Users user={user} key={user.id} />
--- Module 6/routers/src/common/Loader.jsx
+++ Module 6/routers/src/common/Loader.jsx
@@ -2,7 +2,7 @@ import "../common/Loader.css";
 
 function Loader() {
   return (
-    <div class="lds-roller">
+    <div className="lds-roller">
       <div></div>
       <div></div>
       <div></div>
