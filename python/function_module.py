6c7df08a478428582d881a0478dc1711e532c83d
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-06-27 17:12:46 +0530

    function module complete

---
+++ python/function_module.py
@@ -0,0 +1,13 @@
+def fibonacci(num):
+    # base case of recurssion
+    if(num==0 or num==1):
+        return num
+    return fibonacci(num-2) + fibonacci(num-1)
+
+def greet(name):
+    print(f"Welcome to you  {name}")
+
+def factorial(num):
+    if(num==0):
+        return 1
+    return num * factorial(num-1)
--- python/main.py
+++ python/main.py
@@ -1,3 +1,4 @@
+import function_module
 # day 1
 
 # print('--- INTRODUCTION ----')
@@ -455,6 +456,163 @@
 # print(f"my name is {name} and my age is {age}.")  
 
 # -------------------------------------------#   
+
+# ------------------------------------------
+# FUNCTION AND MODULES
+#  function define ,calling,parameter,argument,return the value ,default value,positonal args,keyword args
+
+def greet(name):
+    print(f"Welcome to you .. {name}")
+
+greet("kamlesh")
+greet("Deepak")
+greet("Suman")
+
+# #  To calculate avarage 
+
+def avarage(num1,num2,num3):
+    sum= (num1+num2+num3)/3
+    print(f"The avarage of {num1},{num2},{num3} is {sum}")
+
+avarage(2,3,4)
+avarage(5,2,1)
+avarage(20,21,12)
+
+# # -------------------------------
+# # FUNCTIONAL ARGUMENT AND RETURN VALUE
+
+def add(a,b):
+    sum = a+b
+    print(f"The sum of two number {a} and {b} : {sum}")
+
+add(5,6)
+add(10,6)
+add(15,16)
+# # ---------- Return the value from -------
+
+def add(a,b):
+    return a+b
+    # sum = a+b
+    # return sum
+
+print(f"The sum of two number is : {add(8,9)}")
+
+# ----------------------------------
+# default parameter
+
+def greet(name = "Guest"):
+    print(f"Welcome to you .. {name}")
+
+greet("kamlesh")
+greet() # default parameter 
+
+# -------------------------------------
+
+def student(name ="Guest", age="20"):
+    print(f"Name:{name},Age:{age}")
+
+student(age=20,name="kamlesh")
+student()
+student("Deepak",36)
+
+# ------------------------------
+# LAMDA FUNCTION => one liner function in python anonymous inline function
+
+square = lambda num: num*num
+print(f"The square of number is {square(4)}")
+
+print(square(5))
+print(square(8))
+
+# #  another lambda function
+add = lambda a,b: a+b
+
+print(add(5,6))
+print(f"The sum of two number is : {add(11,9)}")
+
+# #  another example
+
+number = [1,2,3,4,5,6,7,8,9]
+square = list(map(lambda x: x**2,number))
+
+print(square)
+
+# -------------------------------------
+# Recursion in python
+
+#  0 1 1 2 3 5 8 13 21
+#  0 1 2 3 4 5 6  7  8
+
+def fibonacci(num):
+    # base case of recurssion
+    if(num==0 or num==1):
+        return num
+    return fibonacci(num-2) + fibonacci(num-1)
+
+print(f"Fibonacci seriese :  {fibonacci(8)}")
+print(f"Fibonacci seriese :  {fibonacci(6)}")
+
+# --------------------------------
+# for factorial using recursion
+
+# 5 = 5 * 4 * 3 * 2 * 1
+# n = n * factorial(n-1)
+
+def factorial(num):
+    if(num==0):
+        return 1
+    return num * factorial(num-1)
+
+print(f"The factorial of number is  :  {factorial(5)}")
+
+# ------------------------------------------------------
+# **************************************
+# MODULES AND pip using external libraries
+
+# --- Build in modules in python
+
+import math
+
+print(math.sqrt(4))
+print(math.ceil(5.91))
+print(math.floor(5.01))
+
+# ----we can create and import our modules 
+
+# import function_module
+
+print(function_module.factorial(5))
+# response = requests.get("https://www.google.com")
+# print(response.text)
+
+#  pip install pandas
+
+# ------------------------------------------
+# FUNCTION SCOPE AND GLOBAL SCOPE AND LIFETIME
+
+# def addation(num1,num2):
+#     """ * num1 and num2 ,z are local variable 
+#         * they are destyed after return the 
+#         * global keyword is used to modify the global variable.
+#     """
+#     c= num1 +num2
+#     z =5
+#     global a
+#     a =0
+
+
+#     return c
+
+# z = 10
+# a=1
+# print(addation(5,9))
+# print(z)
+# print(a)
+
+# # docstring (writing function Documentation ......)
+# print(addation.__doc__)
+
+
     
     
     
