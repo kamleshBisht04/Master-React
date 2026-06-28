704892f9af489d33f988198b1ab9782ef72dd77e
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-06-28 21:54:32 +0530

    starting object oriented concepts in python

--- python/main.py
+++ python/main.py
@@ -736,40 +736,49 @@
 
 # dictonary
 
-student ={"name":"kamlesh",1:"Delhi","age":28,"grade":'A'}
-print(student)
-#  accessing the dictonary via key
-print(student["name"])
-print(student[1])
-print(student["age"])
-print(student["grade"])
+# student ={"name":"kamlesh",1:"Delhi","age":28,"grade":'A'}
+# print(student)
+# #  accessing the dictonary via key
+# print(student["name"])
+# print(student[1])
+# print(student["age"])
+# print(student["grade"])
 
-# modify the key value pair
-student["age"]=32
-print(student["age"])
+# # modify the key value pair
+# student["age"]=32
+# print(student["age"])
 
 
 # Adding new properties key value pair
 
-student["city"]= "Ranikhet"
-print(student)
+# student["city"]= "Ranikhet"
+# print(student)
 
 
-#  common dictonary method 
+# #  common dictonary method 
 
-print(student.keys())
-print(student.values())
-print(student.items())
+# print(student.keys())
+# print(student.values())
+# print(student.items())
 
-student.pop("age")
-# student.clear()
-print(student)
+# student.pop("age")
+# # student.clear()
+# print(student)
 
-#  dictonary comperhension
+# #  dictonary comperhension
 
-square = {x : x**2 for x in range(6)}
-print(square)
+# square = {x : x**2 for x in range(6)}
+# print(square)
 
-# list 
-square = [x**2 for x in range(6)]
-print(square)
+# # list 
+# square = [x**2 for x in range(6)]
+# print(square)
+
+# *************************************************
+# *************************************************
+
+# object oriented programming
+
+
+
+
